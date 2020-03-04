3279c0a44c50349669ac9311ce036f6eedebea29
Aguilar, Julio <Julio.Aguilar@meijer.com>
2020-03-03 21:47:49 -0500

    Docker command updated (don't remove after container stopped)

--- start_docker.sh
+++ start_docker.sh
@@ -1,1 +1,1 @@
-docker run -it --rm --entrypoint "/run.sh" -p 8888:8888 -v ${pwd}:/src udacity/carnd-term1-starter-kit
+docker run -it --entrypoint "/run.sh" -p 8888:8888 -v ${pwd}:/src udacity/carnd-term1-starter-kit
